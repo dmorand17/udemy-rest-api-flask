9b2a800eb47f8021a36f45125b86bad21cc5e468
Doug Morand <dmorand@gmail.com>
2023-01-12 03:46:19 +0000

    devcontainer fixes

--- devcontainer-postInstall.sh
+++ devcontainer-postInstall.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 set -eo
 
-# install fzf
-echo "Installing fzf..."
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --no-update-rc --key-bindings --completion
-
-# install npm and nodejs
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-# export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-# nvm install --lts
-
 # Setup git user.email
 git config --global user.email "dmorand@gmail.com"
 git config --global user.name "Doug Morand"
